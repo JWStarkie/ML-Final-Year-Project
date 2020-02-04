da43678dc2c1f68a21f4a65d7ecb378bb5def699
JWStarkie <jstarkie@live.co.uk>
2020-02-04 18:14:36 +0000

    Conditional statement for takePicture functionality, babel package installed to manage files and folder imports.

--- components/ML.js
+++ components/ML.js
@@ -3,13 +3,27 @@
 import React from "react";
 import { Button, View } from "react-native";
 
+import NavigationService from "utils/NavigationService.js";
+
 export default class ML extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Button
           title="Camera Set Up"
-          onPress={() => this.props.navigation.navigate("CameraFunction")}
+          onPress={() =>
+            NavigationService.navigate("CameraFunction", {
+              takePictureFunction: false
+            })
+          }
+        />
+        <Button
+          title="Camera Set Up & Take Picture"
+          onPress={() =>
+            NavigationService.navigate("CameraFunction", {
+              takePictureFunction: true
+            })
+          }
         />
       </View>
     );
--- utils/CameraFunction.js
+++ utils/CameraFunction.js
@@ -11,24 +11,40 @@ import NavigationService from "./NavigationService";
 export default class CameraFunction extends Component {
   state = {
     hasPermission: null,
-    type: Camera.Constants.Type.back
+    type: Camera.Constants.Type.back,
+    takePictureFunction: null
   };
 
   async componentDidMount() {
     const { status } = await Camera.requestPermissionsAsync();
+    const { navigation } = this.props;
     this.setState({ hasPermission: status === "granted" });
+    this.setState({
+      takePictureFunction: navigation.state.params.takePictureFunction
+    });
   }
 
   render() {
     const { hasPermission } = this.state;
+    // const { navigation } = this.props;
     if (hasPermission === null) {
       return <View />;
     } else if (hasPermission === false) {
       return (
         <View
-          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center"
+          }}
         >
-          <Text style={{ textAlign: "center", color: "#333", marginBottom: 5 }}>
+          <Text
+            style={{
+              textAlign: "center",
+              color: "#333",
+              marginBottom: 5
+            }}
+          >
             No authorisation to access to camera, please give this application
             permission to continue!! You must now do this through your setting
             or restart the application!!
@@ -54,7 +70,7 @@ export default class CameraFunction extends Component {
                 margin: 25
               }}
             >
-              <TouchableOpacity
+              {/* <TouchableOpacity
                 style={{
                   alignSelf: "flex-end",
                   alignItems: "center",
@@ -71,7 +87,38 @@ export default class CameraFunction extends Component {
                     padding: 10
                   }}
                 />
-              </TouchableOpacity>
+              </TouchableOpacity> */}
+              {this.state.takePictureFunction ? (
+                <View
+                  style={{
+                    flex: 1,
+                    backgroundColor: "transparent",
+                    flexDirection: "row",
+                    justifyContent: "center"
+                  }}
+                >
+                  <TouchableOpacity
+                    style={{
+                      flex: 1,
+                      justifyContent: "flex-end",
+                      alignItems: "center",
+                      backgroundColor: "transparent",
+                      marginBottom: 5
+                    }}
+                    onPress={this.takePicture}
+                  >
+                    <FontAwesome
+                      name="camera"
+                      style={{
+                        color: "#fff",
+                        fontSize: 40,
+                        backgroundColor: "#000",
+                        padding: 10
+                      }}
+                    />
+                  </TouchableOpacity>
+                </View>
+              ) : null}
             </View>
           </Camera>
         </View>
@@ -83,7 +130,9 @@ export default class CameraFunction extends Component {
       await this.camera
         .takePictureAsync({ skipProcessing: true })
         .then(data => {
-          NavigationService.navigate("ImagePreview", { imageUri: data.uri });
+          NavigationService.navigate("ImagePreview", {
+            imageUri: data.uri
+          });
         });
     }
   };
